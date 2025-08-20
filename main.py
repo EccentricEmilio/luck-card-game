e3b56c46ac5ad896a1e498fe7828debc6aa6cf29
emilio <99819072+EccentricEmilio@users.noreply.github.com>
2025-08-20 22:03:02 +0200

    create main.py and started on logic

---
+++ main.py
@@ -0,0 +1,16 @@
+class game:
+  def __init__(self, is_game, player_amount, ):
+    
+    pass
+
+  def ask(self, question):
+    answer = input(question)
+    if answer == "exit":
+      self.is_game = False
+
+
+
+def main():
+  is_game = True 
+  while is_game:
+
